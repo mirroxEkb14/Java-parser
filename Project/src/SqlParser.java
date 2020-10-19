1d9357bd67405ffef73e5e3c3ce6064ae15577b5
Daniyar Am <dancho.rancho07@gmail.com>
2020-10-20 02:26:25 +0600

    next page left

--- Project/src/SqlParser.java
+++ Project/src/SqlParser.java
@@ -4,133 +4,142 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import java.util.Scanner;
 
-/*
+import java.util.Map;
+import java.util.HashMap;
 
- */
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 
 public class SqlParser {
     public static final String URL = "https://www.sql.ru/forum/job-offers"; // url constant
+    public static Map<String, String> months; // month names
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\nEnter url(sql.ru), key word(java), period(months)");
+        System.out.println("\nYou have too much free time? Well, let`s find you a new job! :)\nEnter url(sql.ru), keyword, period(months)");
 
+        String input;
         while (true) {
-            if (checkInput(scanner.nextLine())) {
-                System.out.println("worked");
+            if (checkInput((input = scanner.nextLine()))) {
+                getVacancy(input);
+                break;
+            } else if (input.equals("q") || input.equals("Q")) {
                 break;
             }
         }
-
-
-//        try {
-//            List<Vacancy> vacancyList = new ArrayList<>(); // create a list for our vacancies
-//
-//            Document doc = Jsoup.connect(URL).get(); // connect to the website
-//
-//            // get vacancy title, vacancy url and vacancy date
-//            Elements tdElements = doc.getElementsByAttributeValue("class", "postslisttopic"); // now 'tdElements' has all vacancies on the page
-//            Elements tbodyElements = doc.getElementsByTag("tbody"); // find all 'tbody' tags (to get date)
-//            int count = 1; // counter for date
-//            for (Element tdElement: tdElements) { // for each vacancy we get its url, title and date
-//                String url = String.valueOf(tdElement.child(0).attr("href"));
-//                String title = tdElement.child(0).text();
-//                String date = tbodyElements.get(2).child(count).child(5).text();
-//
-//                vacancyList.add(new Vacancy(url, title, date)); // put it in the list
-//
-//                count++;
-//            }
-//
-//        } catch (IOException e) {
-//            System.out.println("\nSome input-output Exception: " + e.getMessage());
-//        }
     }
 
     public static boolean checkInput(String input) {
-        boolean isCorrect = false;
+        boolean isCorrect = true;
 
+        input = input.replaceAll(" ", "");
         String[] inputArray = input.split(",");
-        if (inputArray.length != 3) {
-            System.out.println("\nNeeded format: url, key word, period(months) - with ','");
+        if (input.equals("q") || input.equals("Q")) {
+            System.out.println("\nbye-bye");
             isCorrect = false;
 
-        } else if (!inputArray[0].toLowerCase().equals("sql") || !inputArray[0].toLowerCase().equals("sql.ru")) {
+        } else if (inputArray.length != 3) {
+            System.out.println("\nNeeded format: url(sql.ru), keyword(java), period(months) - with ','");
+            isCorrect = false;
+
+        } else if (!inputArray[0].toLowerCase().equals("sql.ru")) {
             System.out.println("\nWrong url\nExample: sql.ru");
             isCorrect = false;
 
-        } else if (Integer.parseInt(inputArray[3]) > 12) {
+        } else if (Integer.parseInt(inputArray[2]) > 12 || Integer.parseInt(inputArray[2]) < 1) {
             System.out.println("\nPeriod must be in months");
             isCorrect = false;
-
-        } else if (inputArray.length == 3) {
-            isCorrect = true;
 
         }
 
         return isCorrect;
     }
+
+    public static void getVacancy(String input) {
+        try {
+            input = input.replaceAll(" ", ""); // from 'input' we need key word and period
+            String[] inputArray = input.split(",");
+
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("MMM");
+
+            File file = new File("C:/Users/Daniyar/Desktop/sqlru.txt"); // our text file
+            PrintWriter pw = new PrintWriter(file);
+
+            listOfMonths(); // create a list of month names
+
+            int monthCounter = 0; // when the month changes
+            int pageCounter = 1; // when the page changes
+            while (monthCounter != Integer.parseInt(inputArray[2])) {
+                Document doc = Jsoup.connect(URL + "/" + pageCounter).get(); // connect to the page
+
+                Elements tbodyElements = doc.getElementsByTag("tbody"); // find all 'tbody' tags that contain the date
+
+                Elements tdElements = doc.getElementsByAttributeValue("class", "postslisttopic"); // now 'tdElements' has all vacancies on the page
+
+                int vacancyCounter = 4; // the first 4 rows in the table on the site are not vacancies, so we start with 4th row
+                int counter = 0; // skip the first 4 rows in the table (they are not vacancies)
+                for (Element tdElement: tdElements) { // take each row(vacancy) in the table on the site
+                    if (vacancyCounter < tbodyElements.get(2).childrenSize()) { // 'tbodyElements.get(2).childrenSize()' - the number of vacancies except the first 4 rows
+                        String[] vacancyDate = tbodyElements.get(2).child(vacancyCounter).child(5).text().split(" ");
+
+                        if (counter < 3) { // skip the first 4 rows
+                            counter++;
+                        } else {
+                            // verification: if we get the next month
+                            if (!vacancyDate[0].equals("сегодня,") && !vacancyDate[0].equals("вчера,") && !vacancyDate[1].equals(months.get(sdf.format(calendar.getTime())))) {
+                                monthCounter++;
+                                calendar.add(Calendar.MONTH, -1);
+
+                                // when the last month the user wants ends: break
+                                if (monthCounter == Integer.parseInt(inputArray[2])) {
+                                    break;
+                                }
+                            }
+
+                            String[] words = tdElement.text().split(" "); // find a vacancy by keyword
+                            for (String word: words) {
+                                if (word.equalsIgnoreCase(inputArray[1])) {
+                                    if (!file.exists()) { // if the file does not exist
+                                        new FileWriter(file);
+                                    }
+
+                                    // write to file
+                                    pw.println(tdElement.child(0).attr("href"));
+                                    pw.println(tdElement.child(0).text());
+                                    pw.println(tbodyElements.get(2).child(vacancyCounter).child(5).text() + "\n");
+                                }
+                            }
+                            vacancyCounter++;
+                        }
+                    }
+                }
+                pageCounter++;
+            }
+            pw.close();
+            System.out.println("\nAll the vacancies have been written to the file");
+
+        } catch (IOException e) {
+            System.out.println("\nSome input-output Exception: " + e.getMessage());
+        }
+    }
+
+    public static void listOfMonths() {
+        // this method is necessary because we need to keep track of the months the user wants
+        months = new HashMap<>();
+
+        String[] monthNumbersInCalendar = {"янв.", "февр.", "март", "апр.", "май", "июнь", "июль", "авг.", "сент.", "окт.", "нояб.", "дек."};
+        String[] monthNamesOnSite = {"янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"};
+
+        for (int i = 0; i < 12; i++) {
+            months.put(monthNumbersInCalendar[i], monthNamesOnSite[i]);
+        }
+    }
 }
-
-class Vacancy {
-    private String url;
-    private String title;
-    private String date;
-
-    public Vacancy(String url, String title, String date) {
-        this.url = url;
-        this.title = title;
-        this.date = date;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    @Override
-    public String toString() {
-        return "Vacancy{" +
-                "url='" + url + '\'' +
-                ", title='" + title + '\'' +
-                ", date='" + date + '\'' +
-                '}';
-    }
-}
-
-//            for (Vacancy vacancy: vacancyList) { // or: vacancyList.forEach(System.out::println);
-//                System.out.println(vacancy);
-//            }
-//
-//            for (Vacancy vacancy: vacancyList) {
-//                String url = vacancy.getUrl();
-//                String title = vacancy.getTitle();
-//                String date = vacancy.getDate();
-//
-//                System.out.printf("Vacancy title: %s\nVacancy url: %s\nVacancy date: %s\n\n", title, url, date);
-//            }
