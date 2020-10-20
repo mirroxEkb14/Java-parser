a75a6ff01cc4f5ef8e18cb150b06a3893968db9b
Daniyar Am <dancho.rancho07@gmail.com>
2020-10-20 23:44:11 +0600

    completed

--- Project/src/SqlParser.java
+++ Project/src/SqlParser.java
@@ -20,6 +20,7 @@ import java.io.PrintWriter;
 public class SqlParser {
     public static final String URL = "https://www.sql.ru/forum/job-offers"; // url constant
     public static Map<String, String> months; // month names
+    public static int inputMonths; // the number of months the user entered
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -41,23 +42,34 @@ public class SqlParser {
 
         input = input.replaceAll(" ", "");
         String[] inputArray = input.split(",");
-        if (input.equals("q") || input.equals("Q")) {
+        if (input.equals("q") || input.equals("Q")) { // if 'q' - quit the program
             System.out.println("\nbye-bye");
             isCorrect = false;
 
-        } else if (inputArray.length != 3) {
-            System.out.println("\nNeeded format: url(sql.ru), keyword(java), period(months) - with ','");
+        } else if (inputArray.length != 4 && inputArray.length != 3) { // check the input format
+            System.out.println("\nNeeded format: url(sql.ru), keyword(java, javascript), period(months) - with ','");
             isCorrect = false;
 
-        } else if (!inputArray[0].toLowerCase().equals("sql.ru")) {
+        } else if (!inputArray[0].equalsIgnoreCase("sql.ru")) { // check url
             System.out.println("\nWrong url\nExample: sql.ru");
             isCorrect = false;
 
-        } else if (Integer.parseInt(inputArray[2]) > 12 || Integer.parseInt(inputArray[2]) < 1) {
-            System.out.println("\nPeriod must be in months");
-            isCorrect = false;
+        } else {
+            // check if the number of months is correct
+            try { // if there is one keyword
+                if (Integer.parseInt(inputArray[2]) > 12 || Integer.parseInt(inputArray[2]) < 1) {
+                    System.out.println("\nPeriod must be in months");
+                    isCorrect = false;
+                }
+                inputMonths = Integer.parseInt(inputArray[2]);
+            } catch (NumberFormatException e) { // if there are two keyword
+                if (Integer.parseInt(inputArray[3]) > 12 || Integer.parseInt(inputArray[3]) < 1) {
+                    System.out.println("\nPeriod must be in months");
+                    isCorrect = false;
+                }
+                inputMonths = Integer.parseInt(inputArray[3]);
+            }
         }
-
         return isCorrect;
     }
 
@@ -68,7 +80,7 @@ public class SqlParser {
             input = input.replaceAll(" ", ""); // from 'input' we need key word and period
             String[] inputArray = input.split(",");
 
-            Calendar calendar = Calendar.getInstance();
+            Calendar calendar = Calendar.getInstance(); // to get the current month and then the previous ones
             SimpleDateFormat sdf = new SimpleDateFormat("MMM");
 
             File file = new File("C:/Users/Daniyar/Desktop/sqlru.txt"); // our text file
@@ -78,50 +90,47 @@ public class SqlParser {
 
             int monthCounter = 0; // when the month changes
             int pageCounter = 1; // when the page changes
-            while (monthCounter != Integer.parseInt(inputArray[2])) {
+
+            /*
+                the vacancies on the site are not in order of dates, so I decided that for each month we`ll look through two pages:
+                for the current month - the first 2 pages
+                for the previous month - the first 4 pages etc.
+             */
+            int pageCeil = 3;
+            while (monthCounter != inputMonths) {
                 Document doc = Jsoup.connect(URL + "/" + pageCounter).get(); // connect to the page
 
                 Elements tbodyElements = doc.getElementsByTag("tbody"); // find all 'tbody' tags that contain the date
 
                 Elements tdElements = doc.getElementsByAttributeValue("class", "postslisttopic"); // now 'tdElements' has all vacancies on the page
 
-                int vacancyCounter = 4; // the first 4 rows in the table on the site are not vacancies, so we start with 4th row
-                int counter = 0; // skip the first 4 rows in the table (they are not vacancies)
+                int vacancyCounter = 4; // the first 4 rows in the table on the site are not vacancies, so we start with 5th row
+                int skipCounter = 0; // skip the first 4 rows in the table (these are not vacancies)
                 for (Element tdElement: tdElements) { // take each row(vacancy) in the table on the site
                     if (vacancyCounter < tbodyElements.get(2).childrenSize()) { // 'tbodyElements.get(2).childrenSize()' - the number of vacancies except the first 4 rows
-                        if (counter < 3) { // skip the first 4 rows
-                            counter++;
+                        if (skipCounter < 3) { // skip the first 4 rows
+                            skipCounter++;
                         } else {
-                            String[] words = tdElement.text().split(" "); // find a vacancy by keyword
+                            String[] words = tdElement.text().replaceAll("[^a-zA-Zа-яА-Я ]", " ").split(" "); // find a vacancy by keyword
                             for (String word: words) {
-                                if (word.equalsIgnoreCase(inputArray[1])) {
-
-                                    System.out.println(java.util.Arrays.toString(words));
-
+                                if (word.equalsIgnoreCase(inputArray[1]) || word.equalsIgnoreCase(inputArray[2])) {
                                     Document dateDoc = Jsoup.connect(tdElement.child(0).attr("href")).get(); // connect to vacancy page
 
                                     Elements dateTdElements = dateDoc.getElementsByAttributeValue("class", "msgFooter"); // get date
                                     String[] vacancyDate = dateTdElements.text().split(" ");
 
-                                    System.out.println(java.util.Arrays.toString(vacancyDate));
-
-                                    // verification: if we get the next month
-                                    if (!vacancyDate[0].equals("сегодня,") && !vacancyDate[0].equals("вчера,") && !vacancyDate[1].equals(months.get(sdf.format(calendar.getTime())))) {
-                                        monthCounter++;
-                                        calendar.add(Calendar.MONTH, -1);
-
-                                        // when the last month the user wants ends: break
-                                        if (monthCounter == Integer.parseInt(inputArray[2])) {
-                                            break;
-                                        }
-                                    }
-
-                                    if (vacancyDate[0].equals("сегодня,") || vacancyDate[0].equals("вчера,") || vacancyDate[1].equals(months.get(sdf.format(calendar.getTime())))) {
+                                    if (pageCounter >= pageCeil) { // if we reach the ceil
+                                        calendar.add(Calendar.MONTH, -1); // set the previous month from the current
+                                        monthCounter++; // when the vacancies ran out this month
+                                        pageCeil += 2; // add two pages because we set the previous month
+                                        pageCounter = 1; // reset 'pageCounter' because every new month we check from the first page
+                                        break;
+                                    } else if (vacancyDate[0].equals("сегодня,") || vacancyDate[0].equals("вчера,") || vacancyDate[1].equals(months.get(sdf.format(calendar.getTime())))) {
                                         if (!file.exists()) { // if the file does not exist
                                             new FileWriter(file);
                                         }
 
-                                        // write to file
+                                        // write to the file
                                         pw.println(tdElement.child(0).attr("href")); // link
                                         pw.println(tdElement.child(0).text()); // title
                                         pw.println(vacancyDate[0] + " " + vacancyDate[1] + "\n"); // date
@@ -129,7 +138,7 @@ public class SqlParser {
                                 }
                             }
                             vacancyCounter++;
-                            if (monthCounter == Integer.parseInt(inputArray[2])) {
+                            if (pageCounter == pageCeil) {
                                 break;
                             }
                         }
@@ -146,7 +155,14 @@ public class SqlParser {
     }
 
     public static void listOfMonths() {
-        // this method is necessary because we need to keep track of the months the user wants
+        /*
+            keys - month names as in the 'Calendar' class,
+            values - month names as on the site
+
+            the user cannot enter a period less than 1, so first we check the vacancies with the current month,
+            but then, if the period is greater than 1, we need to check the previous months, but their names on the site are
+            different from the names in the 'Calendar' class, so we need to compare them
+         */
         months = new HashMap<>();
 
         String[] monthNumbersInCalendar = {"янв.", "февр.", "март", "апр.", "май", "июнь", "июль", "авг.", "сент.", "окт.", "нояб.", "дек."};
@@ -225,3 +241,32 @@ public class SqlParser {
 //        } catch (IOException e) {
 //        System.out.println("\nSome input-output Exception: " + e.getMessage());
 //        }
+
+// verification: if we get the next month
+//                                    if (!vacancyDate[0].equals("сегодня,") && !vacancyDate[0].equals("вчера,") && !vacancyDate[1].equals(months.get(sdf.format(calendar.getTime())))) {
+//                                        monthCounter++; // when the vacancies ran out this month
+//                                        if (inputMonths != 1) {
+//                                            calendar.add(Calendar.MONTH, -1); // set the previous month from the current
+//                                        }
+//
+//                                        // when the last month the user wants ends: break
+////                                        try { // 'try-catch' blocks are necessary because we do not know in advance if the user enters two or one keyword
+////                                            if (monthCounter == Integer.parseInt(inputArray[2])) {
+////                                                break;
+////                                            }
+////                                        } catch (NumberFormatException e) {
+////                                            if (monthCounter == Integer.parseInt(inputArray[3])) {
+////                                                break;
+////                                            }
+////                                        }
+//                                    }
+
+//                            try { // 'try-catch' blocks are necessary because we do not know in advance if the user enters two or one keyword
+//                                if (monthCounter == Integer.parseInt(inputArray[2])) {
+//                                    break;
+//                                }
+//                            } catch (NumberFormatException e) {
+//                                if (monthCounter == Integer.parseInt(inputArray[3])) {
+//                                    break;
+//                                }
+//                            }
